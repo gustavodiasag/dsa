2a1551f19a6862ba1564192341a48b12480cb97f
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-10-14 17:33:29 -0300

    style: formatting

--- lib/clist/example/page.h
+++ lib/clist/example/page.h
@@ -9,6 +9,12 @@ typedef struct _Page {
     int     _ref;
 } Page;
 
+/**
+ * Finds the page to be replace given an element of a circular list containing
+ * the page at which to begin searching.
+ * 
+ * Returns the number of the page to be replaced.
+ */
 int replace_page(CListElt** current);
 
 #endif
